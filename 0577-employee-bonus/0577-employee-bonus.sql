2b83e58ff72df2a9dde29f3f6658e549a17069a4
vamshiKrishnaReddy20 <vamshi172015@outlook.com>
2024-11-24 23:56:12 +0530

    Time: 833 ms (98.47%), Space: 0B (100%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -1,4 +1,8 @@
 # Write your MySQL query statement below
-SELECT name,bonus FROM Employee AS E
+SELECT
+    name,
+    bonus 
+FROM 
+    Employee AS E
 LEFT JOIN Bonus AS B ON E.empId = B.empId
 WHERE bonus < 1000 IS NOT FALSE;
